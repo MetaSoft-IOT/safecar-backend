67c961de45ebeedc64f790d52689acb3bf25fac1
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-09-13 22:15:55 -0500

    chore: initial commit.

---
+++ src/main/java/com/safecar/platform/backend/SafecarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.safecar.platform.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafecarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafecarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safecar/platform/backend/SafecarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safecar.platform.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafecarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
