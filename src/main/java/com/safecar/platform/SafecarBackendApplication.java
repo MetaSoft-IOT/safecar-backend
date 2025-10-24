6ebfabdd133e6bd84c5b55841e322e81ef2972ac
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-10-23 21:00:35 -0500

    fix: enable jpa auditing to avoid null audit fields.

--- src/main/java/com/safecar/platform/SafecarBackendApplication.java
+++ src/main/java/com/safecar/platform/SafecarBackendApplication.java
@@ -2,8 +2,10 @@ package com.safecar.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SafecarBackendApplication {
 
 	public static void main(String[] args) {
