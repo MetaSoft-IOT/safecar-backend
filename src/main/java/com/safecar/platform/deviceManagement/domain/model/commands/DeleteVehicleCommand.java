1a38f4373fe7bc201818b0a62cc1ab3a8ba7f486
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-25 09:47:09 -0500

    feat: added delete method to command

---
+++ src/main/java/com/safecar/platform/deviceManagement/domain/model/commands/DeleteVehicleCommand.java
@@ -0,0 +1,10 @@
+package com.safecar.platform.deviceManagement.domain.model.commands;
+
+public record DeleteVehicleCommand(
+        Long vehicleId){
+    public DeleteVehicleCommand {
+        if (vehicleId == null || vehicleId <= 0) {
+            throw new IllegalArgumentException("Vehicle ID must be a positive non-null value.");
+        }
+    }
+}
