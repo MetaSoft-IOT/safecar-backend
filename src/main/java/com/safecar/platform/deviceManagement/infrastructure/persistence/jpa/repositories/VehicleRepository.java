43e7c4e9fee84851683d1b1ccad79af090870a31
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-26 09:49:35 -0500

    feat: update VehicleRepository

--- src/main/java/com/safecar/platform/deviceManagement/infrastructure/persistence/jpa/repositories/VehicleRepository.java
+++ src/main/java/com/safecar/platform/deviceManagement/infrastructure/persistence/jpa/repositories/VehicleRepository.java
@@ -7,11 +7,14 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
+
+
 
     List<Vehicle> findByDriverId(Long driverId);
 
-    Optional<Vehicle> findByLicensePlate(String licensePlate);
+
 }
