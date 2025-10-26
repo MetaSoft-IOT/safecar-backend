974f924e762223a57177f8ab6e44bb89bef0cd80
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-26 09:27:02 -0500

    feat: added DeleteVehicleCommand

--- src/main/java/com/safecar/platform/deviceManagement/domain/model/commands/DeleteVehicleCommand.java
+++ src/main/java/com/safecar/platform/deviceManagement/domain/model/commands/DeleteVehicleCommand.java
@@ -1,7 +1,8 @@
 package com.safecar.platform.deviceManagement.domain.model.commands;
 
 public record DeleteVehicleCommand(
-        Long vehicleId){
+        Long vehicleId,
+        Long driverId){
     public DeleteVehicleCommand {
         if (vehicleId == null || vehicleId <= 0) {
             throw new IllegalArgumentException("Vehicle ID must be a positive non-null value.");
