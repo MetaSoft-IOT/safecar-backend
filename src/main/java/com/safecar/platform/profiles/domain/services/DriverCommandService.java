d2abba0fa49951b50b1645b301afffa6c4d165d0
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-26 23:05:14 -0500

    feat: added DriverCommandService

---
+++ src/main/java/com/safecar/platform/profiles/domain/services/DriverCommandService.java
@@ -0,0 +1,13 @@
+package com.safecar.platform.profiles.domain.services;
+
+
+
+import com.safecar.platform.profiles.domain.model.aggregates.Driver;
+import com.safecar.platform.profiles.domain.model.commands.CreateDriverCommand;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface DriverCommandService {
+    Optional<Driver> handle(CreateDriverCommand command, UUID userId);
+}
