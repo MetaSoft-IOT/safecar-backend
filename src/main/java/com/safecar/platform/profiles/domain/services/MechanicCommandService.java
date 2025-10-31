ad97314b7c7c74e44cf21c0a8d5402c6feaa516a
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:42:16 -0500

    feat: added MechanicCommandService

--- src/main/java/com/safecar/platform/profiles/domain/services/MechanicCommandService.java
+++ src/main/java/com/safecar/platform/profiles/domain/services/MechanicCommandService.java
@@ -6,7 +6,6 @@ import com.safecar.platform.profiles.domain.model.aggregates.Mechanic;
 import com.safecar.platform.profiles.domain.model.commands.CreateMechanicCommand;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public interface MechanicCommandService {
     Optional<Mechanic> handle(CreateMechanicCommand command, Long userId);
