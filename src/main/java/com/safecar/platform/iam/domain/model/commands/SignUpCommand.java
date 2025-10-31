a372bcdcebed5fefb88fde18b61a69263900ea96
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:48:36 -0500

    feat: added SignUpCommand

--- src/main/java/com/safecar/platform/iam/domain/model/commands/SignUpCommand.java
+++ src/main/java/com/safecar/platform/iam/domain/model/commands/SignUpCommand.java
@@ -1,5 +1,6 @@
 package com.safecar.platform.iam.domain.model.commands;
 
+import java.util.List;
 import java.util.Set;
 import com.safecar.platform.iam.domain.model.entities.Role;
 
@@ -15,26 +16,6 @@ import com.safecar.platform.iam.domain.model.entities.Role;
  * @since 2025-10-05
  */
 public record SignUpCommand(
-        String email,
-        String password,
-        String confirmPassword,
-        Set<Role> roles) {
-    public SignUpCommand {
-        if (email == null || email.isBlank()) {
-            throw new IllegalArgumentException("Email cannot be null or blank");
-        }
-        if (password == null || password.isBlank()) {
-            throw new IllegalArgumentException("Password cannot be null or blank");
-        }
-        if (confirmPassword == null || confirmPassword.isBlank()) {
-            throw new IllegalArgumentException("Confirm Password cannot be null or blank");
-        }
-        if (!password.equals(confirmPassword)) {
-            throw new IllegalArgumentException("Password and Confirm Password do not match");
-        }
-        if (roles == null || roles.isEmpty()) {
-            throw new IllegalArgumentException("Roles cannot be null or empty");
-        }
-    }
-
+        String username, String password, List<Role> roles
+) {
 }
