ebc05889c9e39af9257f62a9508a8aedea4fd131
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:35:22 -0500

    feat: added AuthenticationController

--- src/main/java/com/safecar/platform/iam/interfaces/rest/AuthenticationController.java
+++ src/main/java/com/safecar/platform/iam/interfaces/rest/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.safecar.platform.iam.interfaces.rest;
 
+import com.safecar.platform.iam.interfaces.rest.resources.*;
+import com.safecar.platform.iam.interfaces.rest.transform.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -13,18 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.safecar.platform.iam.domain.services.UserCommandService;
-import com.safecar.platform.iam.interfaces.rest.resources.AuthenticatedUserResource;
-import com.safecar.platform.iam.interfaces.rest.resources.SignInResource;
-import com.safecar.platform.iam.interfaces.rest.resources.SignUpResource;
-import com.safecar.platform.iam.interfaces.rest.resources.UserResource;
-import com.safecar.platform.iam.interfaces.rest.transform.AuthenticatedUserResourceFromEntityAssembler;
-import com.safecar.platform.iam.interfaces.rest.transform.SignInCommandFromResourceAssembler;
-import com.safecar.platform.iam.interfaces.rest.transform.SignUpCommandFromResourceAssembler;
-import com.safecar.platform.iam.interfaces.rest.transform.UserResourceFromEntityAssembler;
 
 @RestController
 @RequestMapping(value = "/api/v1/authentication", produces = MediaType.APPLICATION_JSON_VALUE)
-@Tag(name = "Authentication", description = "Available Authentication Endpoints")
+@Tag(name = "Authentication", description = "Authentication Endpoints")
 public class AuthenticationController {
     private final UserCommandService userCommandService;
 
@@ -32,36 +26,53 @@ public class AuthenticationController {
         this.userCommandService = userCommandService;
     }
 
-    @PostMapping("/sign-up")
-    @Operation(summary = "Sign up a new user", description = "Sign up a new user with the provided username, password, and roles.")
+    /**
+     * Handles the sign-in request.
+     * @param signInResource the sign-in request body.
+     * @return the authenticated user resource.
+     */
+    @PostMapping("/sign-in")
+    public ResponseEntity<AuthenticatedUserResource> signIn(@RequestBody SignInResource signInResource) {
+        var signInCommand = SignInCommandFromResourceAssembler.toCommandFromResource(signInResource);
+        var authenticatedUser = userCommandService.handle(signInCommand);
+        if (authenticatedUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        var authenticatedUserResource = AuthenticatedUserResourceFromEntityAssembler.toResourceFromEntity(authenticatedUser.get().getLeft(), authenticatedUser.get().getRight());
+        return ResponseEntity.ok(authenticatedUserResource);
+    }
+
+
+    @Operation(summary = "Create Driver")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "User created successfully."),
-            @ApiResponse(responseCode = "400", description = "Bad request.")
+            @ApiResponse(responseCode = "201", description = "Driver created"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
     })
-    public ResponseEntity<UserResource> signUp(@RequestBody SignUpResource resource) {
-        var signUpCommand = SignUpCommandFromResourceAssembler.toCommandFromResource(resource);
+    @PostMapping("/sign-up/driver")
+    public ResponseEntity<UserResource> signUpDriver(@RequestBody SignUpDriverResource signUpDriverResource) {
+        var signUpCommand = SignUpDriverCommandFromResourceAssembler.toCommandFromResource(signUpDriverResource);
         var user = userCommandService.handle(signUpCommand);
-        if (user.isEmpty())
+        if (user.isEmpty()) {
             return ResponseEntity.badRequest().build();
-        var userEntity = user.get();
-        var userResource = UserResourceFromEntityAssembler.toResourceFromEntity(userEntity);
+        }
+        var userResource = UserResourceFromEntityAssembler.toResourceFromEntity(user.get());
         return new ResponseEntity<>(userResource, HttpStatus.CREATED);
     }
 
-    @PostMapping("/sign-in")
-    @Operation(summary = "Sign in a user", description = "Sign in a user with the provided username and password.")
+
+    @Operation(summary = "Create Workshop")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "User signed in successfully."),
-            @ApiResponse(responseCode = "404", description = "User not found.")
+            @ApiResponse(responseCode = "201", description = "Workshop created"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
     })
-    public ResponseEntity<AuthenticatedUserResource> signIn(@RequestBody SignInResource resource) {
-        var signInCommand = SignInCommandFromResourceAssembler.toCommandFromResource(resource);
-        var authenticatedUserResult = userCommandService.handle(signInCommand);
-        if (authenticatedUserResult.isEmpty())
-            return ResponseEntity.notFound().build();
-        var authenticatedUser = authenticatedUserResult.get();
-        var authenticatedUserResource = AuthenticatedUserResourceFromEntityAssembler
-                .toResourceFromEntity(authenticatedUser.left, authenticatedUser.right);
-        return ResponseEntity.ok(authenticatedUserResource);
+    @PostMapping("/sign-up/workshop")
+    public ResponseEntity<UserResource> signUpWorkshop(@RequestBody SignUpWorkshopResource signUpWorkshopResource) {
+        var signUpCommand = SignUpWorkshopCommandFromResourceAssembler.toCommandFromResource(signUpWorkshopResource);
+        var user = userCommandService.handle(signUpCommand);
+        if (user.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        var userResource = UserResourceFromEntityAssembler.toResourceFromEntity(user.get());
+        return new ResponseEntity<>(userResource, HttpStatus.CREATED);
     }
 }
