3333fa460ff780084c6e4da1ff37a72291d0cb6f
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:47:14 -0500

    feat: added RoleRepository

--- src/main/java/com/safecar/platform/iam/infrastructure/persistence/jpa/repositories/RoleRepository.java
+++ src/main/java/com/safecar/platform/iam/infrastructure/persistence/jpa/repositories/RoleRepository.java
@@ -18,21 +18,7 @@ import com.safecar.platform.iam.domain.model.valueobjects.Roles;
  * @since 1.0.0
  */
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-    /**
-     * Finds a role by its name.
-     *
-     * @param name the {@link Roles} value object representing the role name
-     * @return an {@link Optional} containing the found {@link Role}, or empty if not found
-     */
+public interface RoleRepository extends JpaRepository<Role, Long>{
     Optional<Role> findByName(Roles name);
-
-    /**
-     * Checks if a role exists by its name.
-     *
-     * @param name the {@link Roles} value object representing the role name
-     * @return {@code true} if a role with the given name exists, {@code false} otherwise
-     */
     boolean existsByName(Roles name);
 }
