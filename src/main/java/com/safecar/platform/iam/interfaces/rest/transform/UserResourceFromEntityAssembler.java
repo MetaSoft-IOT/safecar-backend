df5c5c5c49fead922cfdd3c4c33c15ff9900e052
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:53:40 -0500

    feat: added UserResourceFromEntityAssembler

---
+++ src/main/java/com/safecar/platform/iam/interfaces/rest/transform/UserResourceFromEntityAssembler.java
@@ -0,0 +1,13 @@
+package com.safecar.platform.iam.interfaces.rest.transform;
+
+import com.safecar.platform.iam.domain.model.entities.Role;
+
+import com.safecar.platform.iam.domain.model.aggregates.User;
+import com.safecar.platform.iam.interfaces.rest.resources.UserResource;
+
+public class UserResourceFromEntityAssembler {
+    public static UserResource toResourceFromEntity(User user) {
+        var roles = user.getRoles().stream().map(Role::getStringName).toList();
+        return new UserResource(user.getId(), user.getEmail(), roles);
+    }
+}
