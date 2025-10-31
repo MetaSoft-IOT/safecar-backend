9f00a3321400569cbd4c9c094a1f278177021408
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:53:12 -0500

    feat: added UserResource

--- src/main/java/com/safecar/platform/iam/interfaces/rest/resources/UserResource.java
+++ src/main/java/com/safecar/platform/iam/interfaces/rest/resources/UserResource.java
@@ -3,17 +3,9 @@ package com.safecar.platform.iam.interfaces.rest.resources;
 import java.util.List;
 import java.util.UUID;
 
-/**
- * Resource representing a user with their ID, username, and role.
- * <p>
- * Used as a response object for user-related endpoints.
- * </p>
- *
- * @param id       the unique identifier of the user
- * @param email    the email of the user
- * @param role     the role assigned to the user
- * @since 1.0.0
- * @author GonzaloQu3dena
- */
-public record UserResource(Long id, String email, List<String> roles) {
+public record UserResource(
+        Long id,
+        String email,
+        List<String> roles
+) {
 }
