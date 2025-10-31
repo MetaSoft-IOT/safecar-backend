b0819ccac778a66bdd6da015679913827d4540ba
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:36:54 -0500

    feat: added ExternalProfileService

--- src/main/java/com/safecar/platform/iam/application/internal/outboundservices/acl/ExternalProfileService.java
+++ src/main/java/com/safecar/platform/iam/application/internal/outboundservices/acl/ExternalProfileService.java
@@ -1,11 +1,8 @@
 package com.safecar.platform.iam.application.internal.outboundservices.acl;
 
 
-
 import com.safecar.platform.profiles.interfaces.acl.ProfilesContextFacade;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 public class ExternalProfileService {
@@ -23,10 +20,10 @@ public class ExternalProfileService {
         );
     }
 
-    public Long createDistributor(String fullName, String city, String country,
-                                  String phone, String companyName, String dni, Long userId){
-        return profileContextFacade.createMechanic(
-                fullName, city, country, phone, companyName, dni, userId
+    public Long createWorkshop(String fullName, String city, String country,
+                               String phone, String companyName, String ruc, Long userId){
+        return profileContextFacade.createWorkshop(
+                fullName, city, country, phone, companyName, ruc, userId
         );
     }
 }
