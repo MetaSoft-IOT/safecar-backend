3fe223db8689354cfa16c17d974ae7464eaacf98
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:48:04 -0500

    feat: added RolesController

--- src/main/java/com/safecar/platform/iam/interfaces/rest/RolesController.java
+++ src/main/java/com/safecar/platform/iam/interfaces/rest/RolesController.java
@@ -1,8 +1,7 @@
 package com.safecar.platform.iam.interfaces.rest;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import com.safecar.platform.iam.domain.model.queries.GetAllRolesQuery;
+import com.safecar.platform.iam.interfaces.rest.transform.RoleResourceFromEntityAssembler;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
@@ -13,10 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.safecar.platform.iam.domain.model.queries.GetAllRolesQuery;
 import com.safecar.platform.iam.domain.services.RoleQueryService;
 import com.safecar.platform.iam.interfaces.rest.resources.RoleResource;
-import com.safecar.platform.iam.interfaces.rest.transform.RoleResourceFromEntityAssembler;
 
 /**
  * Controller to handle role endpoints.
@@ -26,28 +23,20 @@ import com.safecar.platform.iam.interfaces.rest.transform.RoleResourceFromEntity
  */
 @RestController
 @RequestMapping(value = "/api/v1/roles", produces = MediaType.APPLICATION_JSON_VALUE)
-@Tag(name = "Roles", description = "Available Role Endpoints")
+@Tag(name = "Roles", description = "Role Management Endpoints")
 public class RolesController {
     private final RoleQueryService roleQueryService;
 
-    /**
-     * Constructor.
-     *
-     * @param roleQueryService The role query service.
-     */
     public RolesController(RoleQueryService roleQueryService) {
         this.roleQueryService = roleQueryService;
     }
 
     /**
-     * Get all roles.
-     *
-     * @return The list of roles.
+     * Get all roles
+     * @return List of role resources
+     * @see RoleResource
      */
     @GetMapping
-    @Operation(summary = "Get all roles", description = "Get all roles.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Roles retrieved successfully.") })
     public ResponseEntity<List<RoleResource>> getAllRoles() {
         var getAllRolesQuery = new GetAllRolesQuery();
         var roles = roleQueryService.handle(getAllRolesQuery);
