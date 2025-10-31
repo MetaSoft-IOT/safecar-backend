4f4658d091d184903f99e5dab3fec4ba8bf20a99
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:52:59 -0500

    feat: added UserRepository

--- src/main/java/com/safecar/platform/iam/infrastructure/persistence/jpa/repositories/UserRepository.java
+++ src/main/java/com/safecar/platform/iam/infrastructure/persistence/jpa/repositories/UserRepository.java
@@ -1,13 +1,11 @@
 package com.safecar.platform.iam.infrastructure.persistence.jpa.repositories;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.safecar.platform.iam.domain.model.aggregates.User;
-import com.safecar.platform.iam.domain.model.valueobjects.Email;
 
 /**
  * Repository interface for managing {@link UserAggregate} entities.
@@ -19,26 +17,8 @@ import com.safecar.platform.iam.domain.model.valueobjects.Email;
  * @since 1.0.0
  */
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    /**
-     * Finds a user by their email.
-     *
-     * @param email the email of the user to find
-     * @return an {@link Optional} containing the found {@link User}, or empty if not found
-     */
-    Optional<User> findByEmail(Email email);
-
-    /**
-     * Checks if a user exists by their email.
-     *
-     * @param email the email to check for existence
-     * @return {@code true} if a user with the given email exists, {@code false} otherwise
-     */
-    boolean existsByEmail(Email email);
-
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String username);
+    boolean existsByEmail(String username);
     boolean existsById(Long userId);
-
-
-
 }
