1df1955f65b3767d3d2a3c561e8b1c5015440d34
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:48:26 -0500

    feat: added SignInCommandFromResourceAssembler

--- src/main/java/com/safecar/platform/iam/interfaces/rest/transform/SignInCommandFromResourceAssembler.java
+++ src/main/java/com/safecar/platform/iam/interfaces/rest/transform/SignInCommandFromResourceAssembler.java
@@ -1,28 +1,11 @@
 package com.safecar.platform.iam.interfaces.rest.transform;
 
+
 import com.safecar.platform.iam.domain.model.commands.SignInCommand;
 import com.safecar.platform.iam.interfaces.rest.resources.SignInResource;
 
-/**
- * Assembler class for converting {@link SignInResource} objects into {@link SignInCommand} commands.
- * <p>
- * Used to transform API request resources into domain commands for sign-in operations.
- * </p>
- *
- * @author GonzaloQu3dena
- * @since 1.0.0
- */
 public class SignInCommandFromResourceAssembler {
-
-    /**
-     * Converts a {@link SignInResource} into a {@link SignInCommand}.
-     *
-     * @param resource the sign-in resource containing user credentials
-     * @return a {@link SignInCommand} with the provided username and password
-     */
-    public static SignInCommand toCommandFromResource(SignInResource resource) {
-        return new SignInCommand(
-                resource.email(),
-                resource.password());
+    public static SignInCommand toCommandFromResource(SignInResource signInResource) {
+        return new SignInCommand(signInResource.email(), signInResource.password());
     }
 }
