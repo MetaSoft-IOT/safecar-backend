2f1bf9d6dbf7566434bfe1a39810c972c7c98121
HOLGUINUPC <u202220250@upc.edu.pe>
2025-10-30 19:44:34 -0500

    feat: added ProfilesContextFacade

--- src/main/java/com/safecar/platform/profiles/interfaces/acl/ProfilesContextFacade.java
+++ src/main/java/com/safecar/platform/profiles/interfaces/acl/ProfilesContextFacade.java
@@ -1,14 +1,12 @@
 package com.safecar.platform.profiles.interfaces.acl;
 
-import java.util.UUID;
-
 public interface ProfilesContextFacade {
 
 
     Long createDriver(String fullName, String city, String country,
                       String phone, String dni, Long userId);
 
-    Long createMechanic(String fullName, String city, String country,
+    Long createWorkshop(String fullName, String city, String country,
                         String phone, String companyName, String dni, Long userId);
 
     boolean exitsDriverByUserId(Long userId);
