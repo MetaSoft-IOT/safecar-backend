822090c69b8c516ca85610e4571f915b041efbc8
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-11-04 00:19:54 -0500

    feat(workshopOps): add service bay entity.

---
+++ src/main/java/com/safecar/platform/workshopOps/domain/model/entities/ServiceBay.java
@@ -0,0 +1,37 @@
+package com.safecar.platform.workshopOps.domain.model.entities;
+
+import com.safecar.platform.shared.domain.model.entities.AuditableModel;
+import com.safecar.platform.workshopOps.domain.model.aggregates.WorkshopOperation;
+import com.safecar.platform.workshopOps.domain.model.valueobjects.WorkshopId;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "service_bays")
+public class ServiceBay extends AuditableModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String label;
+
+    @Embedded
+    private WorkshopId workshop;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workshop_operation_id")
+    private WorkshopOperation workshopOperation;
+
+    protected ServiceBay() {
+    }
+
+    public ServiceBay(String label, WorkshopId workshop, WorkshopOperation workshopOperation) {
+        this.label = label;
+        this.workshop = workshop;
+        this.workshopOperation = workshopOperation;
+    }
+}
